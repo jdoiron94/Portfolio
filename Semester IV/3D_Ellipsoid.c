92237706f4840344760493563b059c0cd68f1d7b
Jacob <jdoiron@smcm.edu>
2014-08-15 02:38:07 -0400

    Fixed spacing
    It was tabbing beforehand; now it's spaces

--- Semester IV/3D_Ellipsoid.c
+++ Semester IV/3D_Ellipsoid.c
@@ -3,7 +3,7 @@
 #include <GL/glut.h>
 
 #ifndef M_PI
-	#define M_PI 3.14159
+    #define M_PI 3.14159
 #endif
 
 #define SPHERE 1
@@ -17,72 +17,72 @@ const GLfloat ambient[] = {0.4, 0.4, 0.5, 1.0};
 
 void draw_mnm(int latitudes, int longitudes)
 {
-	for (int i = 0; i <= latitudes; i++)
-	{
-		double lat_0[] = {M_PI * (-0.5 + (double) (i - 1) / latitudes), sin(lat_0[0]) / 2.75, cos(lat_0[0])};
-		double lat_1[] = {M_PI * (-0.5 + (double) i / latitudes), sin(lat_1[0]) / 2.75, cos(lat_1[0])};
-		glBegin(GL_QUAD_STRIP);
-			for (int j = 0; j <= longitudes; j++)
-			{
-				double longitude = 2 * M_PI * (double) (j - 1) / longitudes;
-				double coordinate[] = {cos(longitude), sin(longitude)};
-				glNormal3f(coordinate[0] * lat_0[2], coordinate[1] * lat_0[2], lat_0[1]);
-				glVertex3f(coordinate[0] * lat_0[2], coordinate[1] * lat_0[2], lat_0[1]);
-				glNormal3f(coordinate[0] * lat_1[2], coordinate[1] * lat_1[2], lat_1[1]);
-				glVertex3f(coordinate[0] * lat_1[2], coordinate[1] * lat_1[2], lat_1[1]);
-			}
-		glEnd();
-	}
+    for (int i = 0; i <= latitudes; i++)
+    {
+        double lat_0[] = {M_PI * (-0.5 + (double) (i - 1) / latitudes), sin(lat_0[0]) / 2.75, cos(lat_0[0])};
+        double lat_1[] = {M_PI * (-0.5 + (double) i / latitudes), sin(lat_1[0]) / 2.75, cos(lat_1[0])};
+        glBegin(GL_QUAD_STRIP);
+            for (int j = 0; j <= longitudes; j++)
+            {
+                double longitude = 2 * M_PI * (double) (j - 1) / longitudes;
+                double coordinate[] = {cos(longitude), sin(longitude)};
+                glNormal3f(coordinate[0] * lat_0[2], coordinate[1] * lat_0[2], lat_0[1]);
+                glVertex3f(coordinate[0] * lat_0[2], coordinate[1] * lat_0[2], lat_0[1]);
+                glNormal3f(coordinate[0] * lat_1[2], coordinate[1] * lat_1[2], lat_1[1]);
+                glVertex3f(coordinate[0] * lat_1[2], coordinate[1] * lat_1[2], lat_1[1]);
+            }
+        glEnd();
+    }
 }
 
 void init()
 {
-	glNewList(SPHERE, GL_COMPILE);
-		glPushAttrib(GL_ALL_ATTRIB_BITS);
-		glColor3f(1.0, 0.0, 0.0);
-		draw_mnm(25, 25);
-		glPopAttrib();
-	glEndList();
-	glClearColor(0.0, 0.0, 0.0, 1.0);
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_LIGHTING);
-	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambient);
-	glEnable(GL_LIGHT0);
-	glLightfv(GL_LIGHT0, GL_POSITION, position);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse);
-	glEnable(GL_COLOR_MATERIAL);
-	glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+    glNewList(SPHERE, GL_COMPILE);
+        glPushAttrib(GL_ALL_ATTRIB_BITS);
+        glColor3f(1.0, 0.0, 0.0);
+        draw_mnm(25, 25);
+        glPopAttrib();
+    glEndList();
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LIGHTING);
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambient);
+    glEnable(GL_LIGHT0);
+    glLightfv(GL_LIGHT0, GL_POSITION, position);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse);
+    glEnable(GL_COLOR_MATERIAL);
+    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
 }
 
 void display()
 {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glCallList(SPHERE);
-	glRotatef(theta[0], 1.0, 0.0, 0.0);
-	glRotatef(theta[1], 0.0, 1.0, 0.0);
-	glRotatef(theta[2], 0.0, 0.0, 1.0);
-	glutSwapBuffers();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glCallList(SPHERE);
+    glRotatef(theta[0], 1.0, 0.0, 0.0);
+    glRotatef(theta[1], 0.0, 1.0, 0.0);
+    glRotatef(theta[2], 0.0, 0.0, 1.0);
+    glutSwapBuffers();
 }
 
 void idle()
 {
-	int time = glutGet(GLUT_ELAPSED_TIME);
-	if (time - last_time >= 200)
-	{
-		int direction = rand() % 3;
-		theta[direction] = (theta[direction] + 3) % 360;
-		last_time = glutGet(GLUT_ELAPSED_TIME);
-		glutPostRedisplay();
-	}
+    int time = glutGet(GLUT_ELAPSED_TIME);
+    if (time - last_time >= 200)
+    {
+        int direction = rand() % 3;
+        theta[direction] = (theta[direction] + 3) % 360;
+        last_time = glutGet(GLUT_ELAPSED_TIME);
+        glutPostRedisplay();
+    }
 }
 
 int main(int argc, char** argv)
 {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutCreateWindow("3D M&M");
-	init();
-	glutIdleFunc(idle);
-	glutDisplayFunc(display);
-	glutMainLoop();
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutCreateWindow("3D M&M");
+    init();
+    glutIdleFunc(idle);
+    glutDisplayFunc(display);
+    glutMainLoop();
 }
--- Semester IV/Display_Lists.c
+++ Semester IV/Display_Lists.c
@@ -17,50 +17,50 @@ float theta = 0.0f;
 
 void draw_head()
 {
-	glNewList(HEAD, GL_COMPILE);
-		glPushAttrib(GL_CURRENT_BIT);
-		glColor3f(1.0f, 1.0f, 0.0f);
-		glBegin(GL_POLYGON);
-			for (float i = 0.0; i < 360.0f * DEGREES; i += DEGREES)
-			{
-				glVertex2f(sinf(i), cosf(i));
-			}
-		glEnd();
-	glEndList();
+    glNewList(HEAD, GL_COMPILE);
+        glPushAttrib(GL_CURRENT_BIT);
+        glColor3f(1.0f, 1.0f, 0.0f);
+        glBegin(GL_POLYGON);
+            for (float i = 0.0; i < 360.0f * DEGREES; i += DEGREES)
+            {
+                glVertex2f(sinf(i), cosf(i));
+            }
+        glEnd();
+    glEndList();
 }
 
 void draw_eyes()
 {
-	glNewList(EYES, GL_COMPILE);
-		glPushAttrib(GL_CURRENT_BIT);
-		glColor3f(1.0f, 1.0f, 1.0f);
-		glBegin(GL_POLYGON);
-			for(float i = 0.0f; i < 360.0f * DEGREES; i+= DEGREES)
-			{
-				glVertex2f(sinf(i) / 3.0f + 0.45f, cosf(i) / 3.0f + 0.45f);
-			}
-		glEnd();
-		glBegin(GL_POLYGON);
-			for (float i = 0.0f; i < 360.0f * DEGREES; i += DEGREES)
-			{
-				glVertex2f(sinf(i) / 3.0f - 0.45f, cosf(i) / 3.0f + 0.45f);
-			}
-		glEnd();
-	glEndList();
+    glNewList(EYES, GL_COMPILE);
+        glPushAttrib(GL_CURRENT_BIT);
+        glColor3f(1.0f, 1.0f, 1.0f);
+        glBegin(GL_POLYGON);
+            for(float i = 0.0f; i < 360.0f * DEGREES; i+= DEGREES)
+            {
+                glVertex2f(sinf(i) / 3.0f + 0.45f, cosf(i) / 3.0f + 0.45f);
+            }
+        glEnd();
+        glBegin(GL_POLYGON);
+            for (float i = 0.0f; i < 360.0f * DEGREES; i += DEGREES)
+            {
+                glVertex2f(sinf(i) / 3.0f - 0.45f, cosf(i) / 3.0f + 0.45f);
+            }
+        glEnd();
+    glEndList();
 }
 
 void draw_smile()
 {
-	glNewList(SMILE, GL_COMPILE);
-		glPushAttrib(GL_CURRENT_BIT);
-		glColor3f(1.0f, 0.0f, 1.0f);
-		glBegin(GL_POLYGON);
-			for (int i = 90; i < 270; i++)
-			{
-				glVertex2f(sinf(i * DEGREES) / 1.25f, cosf(i * DEGREES) / 1.25f);
-			}
-		glEnd();
-	glEndList();
+    glNewList(SMILE, GL_COMPILE);
+        glPushAttrib(GL_CURRENT_BIT);
+        glColor3f(1.0f, 0.0f, 1.0f);
+        glBegin(GL_POLYGON);
+            for (int i = 90; i < 270; i++)
+            {
+                glVertex2f(sinf(i * DEGREES) / 1.25f, cosf(i * DEGREES) / 1.25f);
+            }
+        glEnd();
+    glEndList();
 }
 
 void draw_frown()
@@ -79,69 +79,69 @@ void draw_frown()
 
 void init()
 {
-	draw_head();
-	draw_eyes();
-	draw_smile();
-	draw_frown();
-	glNewList(SMILEY_FACE, GL_COMPILE);
-		glCallList(HEAD);
-		glCallList(EYES);
-		glCallList(SMILE);
-	glEndList();
-	glNewList(FROWNY_FACE, GL_COMPILE);
-		glCallList(HEAD);
-		glCallList(EYES);
-		glCallList(FROWN);
-	glEndList();
+    draw_head();
+    draw_eyes();
+    draw_smile();
+    draw_frown();
+    glNewList(SMILEY_FACE, GL_COMPILE);
+        glCallList(HEAD);
+        glCallList(EYES);
+        glCallList(SMILE);
+        glEndList();
+    glNewList(FROWNY_FACE, GL_COMPILE);
+        glCallList(HEAD);
+        glCallList(EYES);
+        glCallList(FROWN);
+    glEndList();
 }
 
 void display()
 {
-	glClearColor(0.0f, 0.5f, 0.5f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glCallList(_state == 0 ? SMILEY_FACE : FROWNY_FACE);
-	glFlush();
+    glClearColor(0.0f, 0.5f, 0.5f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glCallList(_state == 0 ? SMILEY_FACE : FROWNY_FACE);
+    glFlush();
 }
 
 void process_key(unsigned char c, int x, int y)
 {
-	switch (c)
-	{
-		case 'f':
-			previous_state = _state;
-			_state = 1;
-			break;
-		case 's':
-			previous_state = _state;
-			_state = 0;
-			break;
-	}
-	if (_state != previous_state)
-	{
-		glutPostRedisplay();
-	}
+    switch (c)
+    {
+        case 'f':
+            previous_state = _state;
+            _state = 1;
+            break;
+        case 's':
+            previous_state = _state;
+            _state = 0;
+            break;
+    }
+    if (_state != previous_state)
+    {
+        glutPostRedisplay();
+    }
 }
 
 void process_mouse(int button, int state, int x, int y)
 {
-	if (state == GLUT_DOWN)
-	{
-		previous_state = _state;
-		_state = _state == 0 ? 1 : 0;
-		if (_state != previous_state)
-		{
-			glutPostRedisplay();
-		}
-	}
+    if (state == GLUT_DOWN)
+    {
+        previous_state = _state;
+        _state = _state == 0 ? 1 : 0;
+        if (_state != previous_state)
+        {
+            glutPostRedisplay();
+        }
+    }
 }
 
 int main(int argc, char** argv)
 {
-	glutInit(&argc, argv);
-	glutCreateWindow("Display Lists");
-	init();
-	glutDisplayFunc(display);
-	glutKeyboardFunc(process_key);
-	glutMouseFunc(process_mouse);
-	glutMainLoop();
+    glutInit(&argc, argv);
+    glutCreateWindow("Display Lists");
+    init();
+    glutDisplayFunc(display);
+    glutKeyboardFunc(process_key);
+    glutMouseFunc(process_mouse);
+    glutMainLoop();
 }
